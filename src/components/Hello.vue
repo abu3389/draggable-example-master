ca95d282674af23914d93afa1946f0e748e22030
abu <294333196@qq.com>
2019-10-11 17:29:37 +0800

    first

---
+++ src/components/Hello.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="fluid container">
+    <div class="form-group form-group-lg panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">Sortable control</h3>
+      </div>
+      <div class="panel-body">
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="editable">拖拽效果</label>
+        </div>
+        <button type="button" class="btn btn-default" @click="orderList">恢复初始排序</button>
+      </div>
+    </div>
+    <!-- 拖拽展示1 -->
+    <div class="col-md-3">
+      <draggable class="list-group" tag="ul" v-model="list" v-bind="dragOptions1" :move="onMove" @start="isDragging=true" @end="isDragging=false">
+        <transition-group type="transition" :name="'flip-list'">
+          <li class="list-group-item" v-for="element in list" :key="element.order">
+            <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
+            {{element.name}}
+            <span class="badge">{{element.order}}</span>
+          </li>
+        </transition-group>
+      </draggable>
+    </div>
+    <!-- 拖拽展示2 -->
+    <div class="col-md-3">
+      <draggable element="span" v-model="list2" v-bind="dragOptions2" :move="onMove">
+        <transition-group name="no" class="list-group" tag="ul">
+          <li class="list-group-item" v-for="element in list2" :key="element.order">
+            <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
+            {{element.name}}
+            <span class="badge">{{element.order}}</span>
+          </li>
+        </transition-group>
+      </draggable>
+    </div>
+    <!-- 数据展示1 -->
+    <div class="list-group col-md-3">
+      <pre>{{listString}}</pre>
+    </div>
+    <!-- 数据展示2 -->
+    <div class="list-group col-md-3">
+      <pre>{{list2String}}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+const message = [
+  "vue.draggable",
+  "draggable",
+  "component",
+  "for",
+  "vue.js 2.0",
+  "based",
+  "on",
+  "Sortablejs"
+];
+
+export default {
+  name: "hello",
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list: message.map((name, index) => {
+        return { name, order: index + 1, fixed: false };
+      }),
+      list2: [],
+      editable: true,
+      isDragging: false,
+      delayedDragging: false
+    };
+  },
+  methods: {
+    orderList() {
+      this.list = this.list.sort((one, two) => {
+        return one.order - two.order;
+      });
+    },
+    onMove({ relatedContext, draggedContext }) {
+      const relatedElement = relatedContext.element;
+      const draggedElement = draggedContext.element;
+      console.log("relatedElement",relatedElement,"draggedElement",draggedElement)
+      return (
+        (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+      );
+    }
+  },
+  computed: {
+    dragOptions1() {
+      return {
+        animation: 0,//ms, 拖动时元素位置变换速度，0无动画
+        group: "description",
+        disabled: !this.editable,
+        ghostClass: "ghost"
+      };
+    },
+    dragOptions2() {
+      return {
+        animation: 0,//ms, 拖动时元素位置变换速度，0无动画
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
+      };
+    },
+    listString() {
+      return JSON.stringify(this.list, null, 2);
+    },
+    list2String() {
+      return JSON.stringify(this.list2, null, 2);
+    }
+  },
+  watch: {
+    isDragging(newValue) {
+      if (newValue) {
+        this.delayedDragging = true;
+        return;
+      }
+      this.$nextTick(() => {
+        this.delayedDragging = false;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.no-move {
+  transition: transform 0s;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.list-group {
+  min-height: 20px;
+}
+
+.list-group-item {
+  cursor: move;
+}
+
+.list-group-item i {
+  cursor: pointer;
+}
+</style>
